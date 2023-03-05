ffba34dd8c5d03dfb0bf8539cd9a6eac68a87d5c
René Hansen <renehh@gmail.com>
2023-03-05 13:23:52 +0100

    Move download and sha check out of Dockerfile
    This change also moves away from passing in the entire url of an NDK, to
    simply passing in the version name e.g. r10e.

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,6 @@
-# List of Android NDK urls for linux-x86_64 along with their sha1 hashes, as
-# listed on:
-#
-# https://developer.android.com/ndk/downloads/older_releases.html
-#
-# All files share the following common url prefix, so that has been omitted in
-# the listing:
-#
-# https://dl.google.com/android/repository/
-#
-# Latest stable version:
-#
-# /android-ndk-r16-linux-x86_64.zip  - b7dcb08fa9fa403e3c0bc3f741a445d7f0399e93
-#
-# Older versions:
-#
-# /android-ndk-r15c-linux-x86_64.zip - 0bf02d4e8b85fd770fd7b9b2cdec57f9441f27a2
-# /android-ndk-r14b-linux-x86_64.zip - becd161da6ed9a823e25be5c02955d9cbca1dbeb
-# /android-ndk-r13b-linux-x86_64.zip - 0600157c4ddf50ec15b8a037cfc474143f718fd0
-# /android-ndk-r12b-linux-x86_64.zip - 170a119bfa0f0ce5dc932405eaa3a7cc61b27694
-# /android-ndk-r11c-linux-x86_64.zip - de5ce9bddeee16fb6af2b9117e9566352aa7e279
-# /android-ndk-r10e-linux-x86_64.zip - f692681b007071103277f6edc6f91cb5c5494a32
-
 FROM ubuntu:latest AS ndk
 
-ARG NDK_URL=https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip
-ARG NDK_SHA=f692681b007071103277f6edc6f91cb5c5494a32
+ARG NDK=r10e
 ARG PLATFORM=android-21
 ARG TOOLCHAIN=arm-linux-androideabi-4.9
 
@@ -34,19 +10,12 @@ RUN apt update && apt -y install \
   unzip \
   python2
 
-RUN wget -O ndk.zip --progress=dot:giga $NDK_URL
+COPY get-ndk.sh make-stc.sh .
 
-RUN echo "$NDK_SHA ndk.zip" | sha1sum -c
+# Download and unzip NDK
+RUN /bin/bash ./get-ndk.sh
 
-RUN unzip -q ndk.zip
-
-COPY make-stc.sh make-stc.sh
-
-#ENV NDK_URL $NDK_URL
-#ENV NDK_SHA $NDK_SHA
-#ENV PLATFORM $PLATFORM
-#ENV TOOLCHAIN $TOOLCHAIN
-
+# Make standalone toolchain
 RUN /bin/bash ./make-stc.sh
 
 # Final stage
---
+++ get-ndk.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# -e  Exit immediately if a command exits with a non-zero status.
+# -u  Treat unset variables as an error when substituting.
+# -x  Print commands and their arguments as they are executed.
+# -o option-name
+#   pipefail     the return value of a pipeline is the status of
+#                the last command to exit with a non-zero status,
+set -euxo pipefail
+
+# List of Android NDK urls for linux-x86_64 along with their sha1 hashes, as
+# listed on:
+#
+# https://developer.android.com/ndk/downloads/older_releases.html
+#
+# All files share the following common url prefix, so that has been omitted in
+# the listing:
+#
+# https://dl.google.com/android/repository/
+#
+# Latest stable version:
+#
+# /android-ndk-r16-linux-x86_64.zip  - b7dcb08fa9fa403e3c0bc3f741a445d7f0399e93
+#
+# Older versions:
+#
+# /android-ndk-r15c-linux-x86_64.zip - 0bf02d4e8b85fd770fd7b9b2cdec57f9441f27a2
+# /android-ndk-r14b-linux-x86_64.zip - becd161da6ed9a823e25be5c02955d9cbca1dbeb
+# /android-ndk-r13b-linux-x86_64.zip - 0600157c4ddf50ec15b8a037cfc474143f718fd0
+# /android-ndk-r12b-linux-x86_64.zip - 170a119bfa0f0ce5dc932405eaa3a7cc61b27694
+# /android-ndk-r11c-linux-x86_64.zip - de5ce9bddeee16fb6af2b9117e9566352aa7e279
+# /android-ndk-r10e-linux-x86_64.zip - f692681b007071103277f6edc6f91cb5c5494a32
+
+declare -A sha_sums
+
+sha_sums["r18b"]="500679655da3a86aecf67007e8ab230ea9b4dd7b"
+sha_sums["r17c"]="12cacc70c3fd2f40574015631c00f41fb8a39048"
+sha_sums["r16b"]="42aa43aae89a50d1c66c3f9fdecd676936da6128"
+sha_sums["r15c"]="0bf02d4e8b85fd770fd7b9b2cdec57f9441f27a2"
+sha_sums["r14b"]="becd161da6ed9a823e25be5c02955d9cbca1dbeb"
+sha_sums["r13b"]="0600157c4ddf50ec15b8a037cfc474143f718fd0"
+sha_sums["r12b"]="170a119bfa0f0ce5dc932405eaa3a7cc61b27694"
+sha_sums["r11c"]="de5ce9bddeee16fb6af2b9117e9566352aa7e279"
+sha_sums["r10e"]="f692681b007071103277f6edc6f91cb5c5494a32"
+
+ndk_url="https://dl.google.com/android/repository/android-ndk-$NDK-linux-x86_64.zip"
+ndk_sha=${sha_sum[$NDK]}
+
+RUN wget -O ndk.zip --progress=dot:giga $ndk_url
+
+RUN echo "$ndk_sha ndk.zip" | sha1sum -c
+
+RUN unzip -q ndk.zip
